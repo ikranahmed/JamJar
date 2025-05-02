e6ac3749567e01d114403fa8b9c8ca6b2a7f7bb0
Zach <zowen17@gmail.com>
2025-05-01 22:48:30 -0400

    fixed all graphql errors and made them work

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jamjar');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/jamjar');
 
 const db = mongoose.connection;
 
